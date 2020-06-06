a03bdd7870e963ebc9b364f5321ad9de77e01e3f
Jeremiah Apple <jeremiahlong777@yahoo.com.sg>
2020-06-06 10:52:12 +0800

    upload image feature (Halfway done but still needs to add firebase)

--- app/src/main/java/sg/edu/np/mad_assignment/TaskPage.java
+++ app/src/main/java/sg/edu/np/mad_assignment/TaskPage.java
@@ -1,10 +1,18 @@
 package sg.edu.np.mad_assignment;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.webkit.MimeTypeMap;
+import android.widget.Button;
+import android.widget.ImageView;
 
 import java.util.ArrayList;
 
@@ -12,7 +20,10 @@ public class TaskPage extends AppCompatActivity {
     RecyclerView recyclerView;
     TaskAdaptor taskAdaptor;
     ArrayList<Task> taskArrayList;
-
+    Button cfb, upimg;
+    ImageView upimgview;
+    //StorageReference mStorageRef;
+    public Uri imguri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,6 +36,57 @@ public class TaskPage extends AppCompatActivity {
         recyclerView.setAdapter(taskAdaptor);
 
         createListData();
+
+        //Start of Upload Image section
+
+
+        cfb = (Button)findViewById(R.id.choosefilebtn); //button for choose file
+        upimg = (Button)findViewById(R.id.uploadimgbtn);//button for upload image
+        upimgview = (ImageView)findViewById(R.id.imageView);//image view for upload image
+        cfb.setOnClickListener(new View.OnClickListener() { //set button for choose file popup
+            @Override
+            public void onClick(View v) {
+                Filechooser(); //calling method for choosing the file
+            }
+        });
+        upimg.setOnClickListener(new View.OnClickListener()
+        {
+            @Override
+            public void onClick(View v) {
+                Fileuploader();
+            }
+        });
+    }
+    //private string to get the extension for the storage to upload
+    private String getExtension(Uri uri)
+    {
+        ContentResolver cr = getContentResolver();
+        MimeTypeMap mimeTypeMap=MimeTypeMap.getSingleton();
+        return mimeTypeMap.getExtensionFromMimeType(cr.getType(uri));
+    }
+    public void Fileuploader()
+    {
+
+    }
+
+    //search from the gallery to select image
+    private void Filechooser() //
+    {
+        Intent intent = new Intent();
+        intent.setType("image/'");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(intent, 1);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==1 && resultCode==RESULT_OK && data!=null)
+        {
+            imguri=data.getData();
+            upimgview.setImageURI(imguri);
+        }
     }
 
     private void createListData(){
