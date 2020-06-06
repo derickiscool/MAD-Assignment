58a5822c1b4db89bd09ce0d6b241b28bcb98809b
Jeremiah Apple <jeremiahlong777@yahoo.com.sg>
2020-06-06 15:07:33 +0800

    removed upload function from TaskPage and made a new file for it called UploadPhoto due to errors, also renamed upload_photo xml

---
+++ app/src/main/java/sg/edu/np/mad_assignment/UploadPhoto.java
@@ -0,0 +1,14 @@
+package sg.edu.np.mad_assignment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UploadPhoto extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upload_photo);
+    }
+}
