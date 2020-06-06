1c46ced75769ebc33d8262dde27cee114dee6f09
Sandra Choy <s10175863@connect.np.edu.sg>
2020-06-06 18:38:10 +0800

    task and achievement items added

--- app/src/main/java/sg/edu/np/mad_assignment/TaskPage.java
+++ app/src/main/java/sg/edu/np/mad_assignment/TaskPage.java
@@ -1,13 +1,11 @@
 package sg.edu.np.mad_assignment;
 
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-
-import android.os.Bundle;
-
 
 import java.util.ArrayList;
 
@@ -38,5 +36,47 @@ public class TaskPage extends AppCompatActivity {
         taskArrayList.add(t2);
         Task t3 = new Task("Watch the news");
         taskArrayList.add(t3);
+        Task t4 = new Task("Clean your room");
+        taskArrayList.add(t4);
+        Task t5 = new Task("Tidy your bed");
+        taskArrayList.add(t5);
+        Task t6 = new Task("Wash your clothes");
+        taskArrayList.add(t6);
+        Task t7 = new Task("Fold clothing");
+        taskArrayList.add(t7);
+        Task t8 = new Task("Organize your belongings");
+        taskArrayList.add(t8);
+        Task t9 = new Task("Clear the mail");
+        taskArrayList.add(t9);
+        Task t10 = new Task("Create a meal plan");
+        taskArrayList.add(t10);
+        Task t11 = new Task("Exercise");
+        taskArrayList.add(t11);
+        Task t12 = new Task("Meditate");
+        taskArrayList.add(t12);
+        Task t13 = new Task("Pick up a new skill");
+        taskArrayList.add(t13);
+        Task t14 = new Task("Learn to cook something new");
+        taskArrayList.add(t14);
+        Task t15 = new Task("Learn a new language");
+        taskArrayList.add(t15);
+        Task t16 = new Task("Learn to play a new musical instrument");
+        taskArrayList.add(t16);
+        Task t17 = new Task("Learn to draw");
+        taskArrayList.add(t17);
+        Task t18 = new Task("Learn to paint");
+        taskArrayList.add(t18);
+        Task t19 = new Task("Wash the dishes");
+        taskArrayList.add(t19);
+        Task t20 = new Task("Compose a song");
+        taskArrayList.add(t20);
+        Task t21 = new Task("Call a friend");
+        taskArrayList.add(t21);
+        Task t22 = new Task("Do a reflection on a piece of paper");
+        taskArrayList.add(t22);
+        Task t23 = new Task("Buy food for the family or yourself");
+        taskArrayList.add(t23);
+        Task t24 = new Task("Encourage a friend with a Gift");
+        taskArrayList.add(t24);
     }
 }
