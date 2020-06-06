4220018fbaa4582f0095ff6d8aeab7f07bd4fe4e
Derick <derickwithoner@gmail.com>
2020-06-06 12:34:09 +0800

    renamed dashboard to categories and added dashboard

--- app/src/main/java/sg/edu/np/mad_assignment/LoginPage.java
+++ app/src/main/java/sg/edu/np/mad_assignment/LoginPage.java
@@ -3,7 +3,6 @@ package sg.edu.np.mad_assignment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -103,8 +102,7 @@ public class LoginPage extends AppCompatActivity{
                     if (passwordFromDB.equals(password) )
                     {
 
-                       Intent dashboard = new Intent(LoginPage.this,Dashboard.class);
-                       startActivity(dashboard);
+                      Intent Dashboard;
 
                     }
                     else{
