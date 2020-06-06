752cefc2575f017d3b138fc20b75ad98ad73eec1
Jeremiah Apple <jeremiahlong777@yahoo.com.sg>
2020-06-06 11:50:44 +0800

    upload image feature complete now linking pages

--- app/src/main/java/sg/edu/np/mad_assignment/TaskPage.java
+++ app/src/main/java/sg/edu/np/mad_assignment/TaskPage.java
@@ -1,5 +1,6 @@
 package sg.edu.np.mad_assignment;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -13,6 +14,14 @@ import android.view.View;
 import android.webkit.MimeTypeMap;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
 
 import java.util.ArrayList;
 
@@ -20,9 +29,10 @@ public class TaskPage extends AppCompatActivity {
     RecyclerView recyclerView;
     TaskAdaptor taskAdaptor;
     ArrayList<Task> taskArrayList;
-    Button cfb, upimg;
+    Button cfb, upimg, gotoup;
     ImageView upimgview;
-    //StorageReference mStorageRef;
+    StorageReference mStorageRef;
+    private StorageTask uploadtask;
     public Uri imguri;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,8 +48,9 @@ public class TaskPage extends AppCompatActivity {
         createListData();
 
         //Start of Upload Image section
+        mStorageRef = FirebaseStorage.getInstance().getReference("Images");
 
-
+        gotoup = (Button)findViewById(R.id.gotouploadbtn); //go to the upload page
         cfb = (Button)findViewById(R.id.choosefilebtn); //button for choose file
         upimg = (Button)findViewById(R.id.uploadimgbtn);//button for upload image
         upimgview = (ImageView)findViewById(R.id.imageView);//image view for upload image
@@ -53,7 +64,13 @@ public class TaskPage extends AppCompatActivity {
         {
             @Override
             public void onClick(View v) {
-                Fileuploader();
+                //validation for if button is pressed more than once
+                if(uploadtask != null && uploadtask.isInProgress()){
+                    Toast.makeText(TaskPage.this, "Upload is in progress",Toast.LENGTH_LONG).show();
+                }
+                else {
+                    Fileuploader();
+                }
             }
         });
     }
@@ -64,9 +81,29 @@ public class TaskPage extends AppCompatActivity {
         MimeTypeMap mimeTypeMap=MimeTypeMap.getSingleton();
         return mimeTypeMap.getExtensionFromMimeType(cr.getType(uri));
     }
+
+    //method to upload file
     public void Fileuploader()
     {
+        StorageReference Ref = mStorageRef.child(System.currentTimeMillis()+"."+getExtension(imguri)); //give the file a name so it will be easier to retrieve
 
+        uploadtask = Ref.putFile(imguri)
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        // Get a URL to the uploaded content
+                        //Uri downloadUrl = taskSnapshot.getDownloadUrl();
+                        Toast.makeText(TaskPage.this,"Image Uploaded Successfully", Toast.LENGTH_LONG).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+                        // Handle unsuccessful uploads
+                        // ...
+                        Toast.makeText(TaskPage.this,"Image failed to upload, please try again.", Toast.LENGTH_LONG).show();
+                    }
+                });
     }
 
     //search from the gallery to select image
--- app/src/main/java/sg/edu/np/mad_assignment/TaskViewHolder.java
+++ app/src/main/java/sg/edu/np/mad_assignment/TaskViewHolder.java
@@ -1,15 +1,18 @@
 package sg.edu.np.mad_assignment;
 
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
 
 public class TaskViewHolder extends RecyclerView.ViewHolder {
     TextView txtTask;
+    Button gotoup;
 
     public TaskViewHolder (View itemView){
         super(itemView);
         txtTask = itemView.findViewById(R.id.textViewTasks);
+        gotoup = itemView.findViewById(R.id.gotouploadbtn);
     }
 }
